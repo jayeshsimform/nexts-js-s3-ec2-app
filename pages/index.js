2fcaaea9135c6e257f643e381dba3ac4291429e9
“Jayesh <jayesh.sojitra@iMac-Pro-5.local>
2022-08-04 12:04:03 +0530

    update favicon

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>AWS Demo App</title>
-        <meta name="description" content="AWS Demo App" />
+        <title>AWS S3 Storage App</title>
+        <meta name="description" content="AWS S3 Storage App" />
       </Head>
       <div className="card mr-5">
         <Link href="/s3/single-part-upload">
